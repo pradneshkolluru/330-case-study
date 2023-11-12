7c6370e1c7c83150b2c3992276ba6bd2a36670ce
Pradnesh Kolluru <pradnesh.kolluru@duke.edu>
2023-11-12 18:37:14 -0500

    used math for distance

--- task1.py
+++ task1.py
@@ -50,7 +50,24 @@ def find_time(start_location, end_location):
 
 def distance(location1, location2):
 
-    return geopy.distance.geodesic(location1, location2).mi
+    from math import sin, cos, sqrt, atan2, radians
+
+    R = 6373.0
+
+    lat1 = radians(location1[0])
+    lon1 = radians(location1[1])
+    lat2 = radians(location2[0])
+    lon2 = radians(location2[1])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
 
 
 
