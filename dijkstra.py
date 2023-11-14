0f843e109c344c60dc0bafef02e4bb8602d0f80d
ameyasr24 <ameyasr24@gmail.com>
2023-11-14 00:16:17 -0500

    dijkstra in progress

---
+++ dijkstra.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+
+# def makeGraph():
+
+
+# def dijkstra(graph, source):
+#     unvisited_nodes = graph.get_nodes()
+#     shortest_path = {}
+#     previous_nodes = {}
+#     max_value = sys.maxsize
+#     #this is like setting the values of each node to be infinity as the starting point
+#     for node in unvisited_nodes:
+#         shortest_path[node] = max_value
+#     #since the starting node distance to itself is 0
+#     shortest_path[source] = 0
+#     while unvisited_nodes:
+
+# def main():
+#     dijkstra()
+
+# if __name__ == "__main__":
+#     main()
