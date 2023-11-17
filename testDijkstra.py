3b4cc157f5f1c3eedd649b7b9ad2a31c75f398d0
Pradnesh Kolluru <pradnesh.kolluru@duke.edu>
2023-11-17 16:21:23 -0500

    emmas stuff added

--- testDijkstra.py
+++ testDijkstra.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import heapq
 import datetime
+from nearest_node import find_closest_coordinate, coordinates
 
 adjacency = {}
 
@@ -124,7 +125,14 @@ def getTimeTraversal(timeStamp):
 
         return (dayType, hour)
 
-
+def closestNodesDijkstra(graph, startcoords, endcoords, timestamp):
+    startcoordsdict = {'lon': startcoords[1], 'lat': startcoords[0]}
+    endcoordsdict = {'lon': endcoords[1], 'lat': endcoords[0]}
+    start = find_closest_coordinate(startcoordsdict, graph)
+    end = find_closest_coordinate(endcoordsdict, coordinates)
+    # print(start)
+    # print(end)
+    return dijkstra(coordinates, start, end, timestamp)
 
 def main():
     #adjacency = {1: [path(2, 5, [40], [40]), path(3, 30, [40], [40])], 2: [path(1, 5, [50], [50]), path(3, 5, [50], [50])], 3:[path(2, 5, [50] ,[50]), path(1, 30, [15], [15])]}
@@ -133,11 +141,14 @@ def main():
     # shortest_distance = dijkstra(adjacency, 39076461, 42847609)
     
     
-    adjacency = {1: [path(2, 5, [40], [40]), path(3, 5, [100], [40])], 2: [path(1, 5, [100], [50]), path(3, 5, [40], [50])], 3:[path(2, 5, [50], [50]), path(1, 30, [15], [15])]}
+    # adjacency = {1: [path(2, 5, [40], [40]), path(3, 5, [100], [40])], 2: [path(1, 5, [100], [50]), path(3, 5, [40], [50])], 3:[path(2, 5, [50], [50]), path(1, 30, [15], [15])]}
 
-    shortest_distance = dijkstra(adjacency, 1, 3, datetime.datetime.strptime("11/20/2023 00:44:00", "%m/%d/%Y %H:%M:%S"))
+    # shortest_distance = dijkstra(adjacency, 1, 3, datetime.datetime.strptime("11/20/2023 00:44:00", "%m/%d/%Y %H:%M:%S"))
 
-    print(shortest_distance)
+    # print(shortest_distance)
+    passengercoords = [-73.935242, 40.655865]
+    drivercoords = [40.667, -73.8713]
+    print(closestNodesDijkstra(coordinates, passengercoords, drivercoords, datetime.datetime.strptime("11/20/2023 00:44:00", "%m/%d/%Y %H:%M:%S")))
 
 
 if __name__ == "__main__":
