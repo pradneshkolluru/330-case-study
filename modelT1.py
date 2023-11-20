74dc259701d6a9f3d443a7f60e58f484792e8504
Pradnesh Kolluru <pradnesh.kolluru@duke.edu>
2023-11-20 18:03:37 -0500

    tested t1 -t3 and implemented efficient t3 code

--- modelT1.py
+++ modelT1.py
@@ -62,7 +62,7 @@ def execModel(verbose = True):
 
       while (len(test.unmatched_passengers) > 0 and len(test.available_drivers) > 0):
             
-            match = test.match1()
+            match = test.match3_inefficient()
 
             if verbose:
                   #print(match)
@@ -71,6 +71,11 @@ def execModel(verbose = True):
 
 
                   print(f"{progress}% Completed")
+
+
+                  if progress > 2:
+
+                        break
 
                   # if progress > 1.0:
                   #       break
