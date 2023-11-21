db16f075d0e779aafbbae4d5e03bd389b75c724a
Pradnesh Kolluru <pradnesh.kolluru@duke.edu>
2023-11-20 23:33:19 -0500

    changed euc distance to use euc distance

--- helperFuncs.py
+++ helperFuncs.py
@@ -183,15 +183,17 @@ def euc_distance(location1, location2):
     lat2 = radians(location2["lat"])
     lon2 = radians(location2["lon"])
 
+    R = 6373.0
+
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    # a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    # c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-    distance = R * c
-    # distance = ((dlon) ** 2 + (dlat ** 2)) ** 0.5
-    # distance = abs(dlon) + abs(dlat)
+    # distance = R * c
+    distance = ((dlon) ** 2 + (dlat ** 2)) ** 0.5
+    #distance = abs(dlon) + abs(dlat)
 
     return distance
 
